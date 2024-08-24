8ddb3c28c8973b7865a5cd643ee597df6f1d8603
hungwei <hungwei@hungweis-MacBook-Pro.local>
2024-08-24 10:59:09 +0900

    New commit

---
+++ icd-Iphone/icd-Iphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  icd-Iphone
+//
+//  Created by hungwei on 2024/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("test Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ icd-Iphone/icd-Iphone/icd_IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  icd_IphoneApp.swift
+//  icd-Iphone
+//
+//  Created by hungwei on 2024/8/24.
+//
+
+import SwiftUI
+
+@main
+struct icd_IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
